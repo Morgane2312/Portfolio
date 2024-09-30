953f3e444bea03afff3f52e696b692d52b0eedd5
morgane2312 <morganelherbier2312@gmail.com>
2024-09-30 14:58:02 +0200

    ok

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Accueil</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="Assets/logo.png"/>
+    <link rel="stylesheet" href="Css/preloader.css">
+    <link rel="stylesheet" href="Css/header.css">
+    <link rel="stylesheet" href="Css/index.css">
+</head>
+
+<body id="pageBody" class="light-mode">
+    <header>
+        <nav>   
+            <ul>
+                <section class='menu-nav'>
+                    <li class="active"><a href="index.html">Accueil</a></li>
+                    <li><a href="projets.php">Mes Projets</a></li>
+                    <li><a href="competences.php">Compétences</a></li>
+                    <section class='rectangle-noir'>
+                    <li class="bouton">
+                        <label class="switch">
+                            <input type="checkbox" id="btn-dark-mode">
+                            <span class="slider round"></span>
+                        </label>
+                    </li>
+                </section>
+                </section>
+                <div class="icons-reseaux">
+                    <li class="icons">
+                        <a href="https://www.instagram.com/morgane_lrb/" target="_blank">
+                        <img id="instagram-icon" src="Assets/Icons/instagram-noir.png" alt="Instagram">
+                        </a>
+                    </li>
+                    <li class="icons">
+                        <a href="https://www.tiktok.com/@" target="_blank">
+                            <img id="tiktok-icon" src="Assets/Icons/tiktok-noir.png" alt="TikTok">
+                        </a>
+                    </li>
+                    <li class="icons">
+                        <a href="https://discord.gg/ffesnXrc8z" target="_blank">
+                            <img id="discord-icon" src="Assets/Icons/discord-noir.png" alt="Discord">
+                        </a>
+                    </li>
+                </div>
+            </ul>
+        </nav>
+    </header>
+    <section class="presentation">
+        <h2>Je suis Morgane</h2>
+        <h1>Développeuse web</h1>
+        <p>Créer des sites web est un plaisir pour moi, laissez moi <br>
+        libérer la magie pour atteindre le résultat escompté</p>
+        <a class="contact" href="mailto:morgane.00paypal@gmail.com">Contactez moi !</a>
+    </section>
+    <section>
+        <p class="droit">© 2024 Tous Droits Réservés</p>
+    </section>
+    <script src="Js/discover.js"></script>
+</body>
+</html>
